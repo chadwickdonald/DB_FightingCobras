baa60960163cbda364f7d8c66e868be7398d87c9
Austin Wang + Paul Zaich <apprentice+aw+pz@devbootcamp.com>
2012-07-11 17:47:48 -0700

    Austin and Paul Wednesday UI stuff

---
+++ spec/ui_spec.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+# Sinatra::Application.environment = :test
+# Bundler.require :default, Sinatra::Application.environment
+require '../src/ui.rb'
+require 'rspec'
+require 'capybara'
+require 'capybara/dsl'
+
+Capybara.app = Sinatra::Application
+
+RSpec.configure do |config|
+  include Capybara::DSL
+end
+
+# def selector string
+#   find :css, string
+# end
+
+describe "ui" do
+  context "homepage" do
+    it "should have a header that states the title of the app" do
+      visit '/'
+      page.should have_content ("I'm not on the page")
+      # find('h1').should have_content "Bart or Drive?"
+      # puts selector('h2').text
+      # selector('h2').has_content?("bart")
+    end
+  end
+end
---
+++ src/config.ru
@@ -0,0 +1,8 @@
+
+# path = File.expand_path "../", __FILE__
+# $LOAD_PATH << (File.expand_path ".") + "/views"
+
+require 'rubygems'
+require 'sinatra'
+require 'ui'
+#run Sinatra::Application
